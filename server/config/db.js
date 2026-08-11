5a2e4dbc230a7d6489c0071e9b2949573997c05a
vignesh <vignesh1515official@gmail.com>
2026-08-11 17:09:56 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -7,7 +7,9 @@ const connectDB = async () => {
     uri = 'mongodb://127.0.0.1:27017/svs_real_estate';
   }
   try {
-    const conn = await mongoose.connect(uri);
+    //const conn = await mongoose.connect(uri);
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error connecting to MongoDB (${uri}): ${error.message}`);
